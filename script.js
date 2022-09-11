cfddcba2713b17354d39777a8e9c07bcdadf0fdf
Saffr3n <saffr3n@gmail.com>
2022-09-12 00:50:13 +0300

    Add grid template customization with user input

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+const btn = document.querySelector('button');
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    const item = document.createElement('div');
-    item.classList.add('item');
-    container.appendChild(item);
+btn.addEventListener('click', () => createGrid(select()));
+
+function createGrid (squares) {
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = `repeat(${squares}, calc(100% / ${squares}))`;
+
+    for (let i = 0; i < squares * squares; i++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        container.appendChild(item);
+    }
+
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => item.style.boxShadow = '1px 1px gray inset');
+    
+    for (let i = squares - 1; i < squares * squares; i += squares)
+        items[i].style.boxShadow = '1px 1px gray inset, -1px 0 gray inset';
+
+    for (let i = squares * squares - 1; i >= squares * (squares - 1); i--)
+        items[i].style.boxShadow = '1px 1px gray inset, 0 -1px gray inset';
+
+    items[squares * squares - 1].style.boxShadow = '1px 1px gray inset, -1px -1px gray inset';
+
+    items.forEach(item => item.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = 'black';
+    }));
 }
 
-const items = document.querySelectorAll('.item');
+function select() {
+    let selection = parseInt(prompt('Choose a desired amount of squares per side (1-100)'));
 
-items.forEach(item => item.addEventListener('mouseover', e => {
-    e.target.style.backgroundColor = 'black';
-}));
+    if (!selection || selection < 1 || selection > 100)
+        selection = select();
+
+    return selection;
+}
+
+createGrid(16);
